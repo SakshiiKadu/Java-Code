d7028384e93fdab4f527874a7b84bee0cc44f0bd
SakshiiKadu <159440341+SakshiiKadu@users.noreply.github.com>
2024-02-16 13:34:37 +0530

    Program To Print Float Value

---
+++ PrintFloatValue.java
@@ -0,0 +1,21 @@
+// This is a simple Java program to demonstrate printing a Float value.
+
+
+
+class PrintFloatValue
+{
+    public static void main(String[] args)
+    {
+        // Declaring and initializing a float variable named 'value' with the value
+        float value = 3.4f;
+
+        System.out.println(value);
+    }
+}
+
+
+
+/*Output:
+ Compile time: Success
+ Run time: 3.4
+ */
