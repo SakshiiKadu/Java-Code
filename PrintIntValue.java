08b6314c281fa34606982584dabe63f466a27839
SakshiiKadu <159440341+SakshiiKadu@users.noreply.github.com>
2024-02-16 13:22:05 +0530

    Program to Print Integer Value

---
+++ PrintIntValue.java
@@ -0,0 +1,19 @@
+// This is a simple Java program to demonstrate printing an integer value
+
+
+
+class PrintIntValue
+{
+public static void main(String [] args)
+{
+	// Declaring and initializing an Integer variable named 'value' with the value
+	int value=10;
+
+	System.out.println(value);
+}
+}
+
+/*output:
+ compile time: success
+ Run time: 10
+ */
