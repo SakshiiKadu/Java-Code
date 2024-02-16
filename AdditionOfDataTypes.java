969e25b42100dc1aa03cf1a23197850525b3e285
Sakshi Kadu <159440341+SakshiiKadu@users.noreply.github.com>
2024-02-16 21:45:03 +0530

    Program For Addition Of Different Data Types

---
+++ AdditionOfDataTypes.java
@@ -0,0 +1,26 @@
+//Java program for addition of different data types.
+
+
+class AdditionOfDataTypes
+{
+    
+    public static void main(String[] aofdt)
+    {
+        // ASCII value of 'A' is 65
+        char a = 'A'; 
+	int b = 3; 
+
+
+        // In Java, when char is involved in arithmetic operations, its ASCII value is used
+        // ASCII value of 'A' (65) + 3 = 68
+
+	System.out.println(a + b); 
+    }
+}
+
+
+
+/* Output:
+   Compile time: Success
+   Run time: 68
+*/
