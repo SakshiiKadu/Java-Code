aea6fadf1f74b3d3a4642c9ec27d25bd88b8e70c
SakshiiKadu <159440341+SakshiiKadu@users.noreply.github.com>
2024-02-16 13:40:44 +0530

    Program To Print Char Value

---
+++ PrintCharValue.java
@@ -0,0 +1,21 @@
+// This is a simple Java program to demonstrate printing a Char value.
+
+
+
+class PrintCharValue
+{
+    public static void main(String[] args)
+    {
+        // Declaring and initializing a Char variable named 'value' with the value
+        char value ='S';
+
+        System.out.println(value);
+    }
+}
+
+
+
+/*Output:
+ Compile time: Success
+ Run time: S
+ */
