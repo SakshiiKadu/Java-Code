cb4ba847cc392a8b49edc9fcd7111708f4b8235e
SakshiiKadu <159440341+SakshiiKadu@users.noreply.github.com>
2024-02-16 13:45:47 +0530

    Program To Print Boolean Value

---
+++ PrintBooleanValue.java
@@ -0,0 +1,21 @@
+// This is a simple Java program to demonstrate printing a Boolean value.
+
+
+
+class PrintBooleanValue
+{
+    public static void main(String[] args)
+    {
+        // Declaring and initializing a Boolean variable named 'value' with the value
+        boolean value =true;
+
+        System.out.println(value);
+    }
+}
+
+
+
+/*Output:
+ Compile time: Success
+ Run time: true
+ */
